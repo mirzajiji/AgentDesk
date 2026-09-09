89905aedd2d09521789850e8458f45d181742ec6
Mirza Jijieshvili <mirzajijieshvili@gmail.com>
2026-09-09 18:55:47 +0400

    Initial Commit

---
+++ AgentDesk/AgentDeskApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgentDeskApp.swift
+//  AgentDesk
+//
+//  Created by Mirza on 09/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct AgentDeskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AgentDeskTests/AgentDeskTests.swift
@@ -0,0 +1,16 @@
+//
+//  AgentDeskTests.swift
+//  AgentDeskTests
+//
+//  Created by Mirza on 09/09/2026.
+//
+
+import Testing
+
+struct AgentDeskTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
